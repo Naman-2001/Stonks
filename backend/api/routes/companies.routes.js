b550bafcfcfcf7bab0369ea3e597e09b8275fc1f
RajatSablok <rajat.main06@gmail.com>
2020-10-31 04:51:52 +0530

    added backend files

---
+++ backend/api/routes/companies.routes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const companyControllers = require("../controllers/companies.controllers");
+
+const router = express.Router();
+
+//Get affected companies
+router.get("/allAffected", companyControllers.getAffectedCompanies);
+
+//Get all companies listed on BSE and NSE
+router.get("/all", companyControllers.getAllCompanies);
+
+module.exports = router;
---
+++ backend/api/routes/crypto.routes.js
@@ -0,0 +1,95 @@
+const express = require("express");
+const CryptoNewsAPI = require("crypto-news-api").default;
+const axios = require("axios").default;
+
+const router = express.Router();
+const api = new CryptoNewsAPI(process.env.CRYPTO_NEWS_API_KEY);
+
+//Get latest crypto news
+router.get("/latestNews", async (req, res) => {
+  api
+    .getLatestNews()
+    .then((articles) => {
+      res.status(200).json(articles);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err.toString(),
+      });
+    });
+});
+
+//Get top crypto news
+router.get("/topNews", async (req, res) => {
+  api
+    .getTopNews()
+    .then((articles) => {
+      res.status(200).json(articles);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err.toString(),
+      });
+    });
+});
+
+//Get top news by coin
+router.get("/getTopNewsByCoin", async (req, res) => {
+  const { coin } = req.query;
+  api
+    .getTopNewsByCoin(coin)
+    .then((articles) => {
+      res.status(200).json(articles);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err.toString(),
+      });
+    });
+});
+
+//Get latest news by coin
+router.get("/getLatestNewsByCoin", async (req, res) => {
+  const { coin } = req.query;
+  api
+    .getLatestNewsByCoin(coin)
+    .then((articles) => {
+      res.status(200).json(articles);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err.toString(),
+      });
+    });
+});
+
+//Get coin details
+router.get("/getCoinDetails", async (req, res) => {
+  const { coin } = req.query;
+  api
+    .getCoinDetails(coin)
+    .then((articles) => {
+      res.status(200).json(articles);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err.toString(),
+      });
+    });
+});
+
+//Get all coins
+router.get("/allCoins", async (req, res) => {
+  axios
+    .get("/https://gemini.com/api/all-prices/1d")
+    .then((response) => {
+      res.status(200).json(response);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err.toString(),
+      });
+    });
+});
+
+module.exports = router;
