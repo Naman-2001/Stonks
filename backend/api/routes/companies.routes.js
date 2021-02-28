84f4c3a0433c1c4ac0177153679a936be4478a6d
RajatSablok <rajat.main06@gmail.com>
2021-02-28 11:21:34 +0530

    get to post poc

--- backend/api/routes/companies.routes.js
+++ backend/api/routes/companies.routes.js
@@ -9,6 +9,6 @@ router.get("/allAffected", companyControllers.getAffectedCompanies);
 //Get all companies listed on BSE and NSE
 router.get("/all", companyControllers.getAllCompanies);
 
-router.get("/proofOfConcept", companyControllers.proofOfConcept);
+router.post("/proofOfConcept", companyControllers.proofOfConcept);
 
 module.exports = router;
